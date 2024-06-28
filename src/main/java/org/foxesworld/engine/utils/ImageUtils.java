06f3519c0f95315cee629ad199de0cd883b92ec6
Aiden <miomoor182@gmail.com>
2024-06-29 00:06:42 +0300

    Image Util Fix

--- src/main/java/org/foxesworld/engine/utils/ImageUtils.java
+++ src/main/java/org/foxesworld/engine/utils/ImageUtils.java
@@ -144,20 +144,12 @@ public class ImageUtils {
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         g2.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
 
-        Shape shape;
-        if (width == height) {
-            shape = new Ellipse2D.Float(0, 0, width, height);
-        } else {
-            shape = new RoundRectangle2D.Float(0, 0, width, height, cornerRadius, cornerRadius);
-        }
-        g2.setClip(shape);
+        g2.setClip(new RoundRectangle2D.Float(0, 0, width, height, cornerRadius, cornerRadius));
         g2.drawImage(image, 0, 0, null);
         g2.dispose();
 
         return roundedImage;
     }
-
-
     public Image getRoundedImage(Image image, int width, int height) {
         BufferedImage roundedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
         Graphics2D g2 = roundedImage.createGraphics();
