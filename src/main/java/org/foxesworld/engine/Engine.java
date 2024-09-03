00cfcff512740a13ecabee80bdf9579dbd800f25
Aiden <miomoor182@gmail.com>
2024-09-03 09:34:44 +0300

    fix x2

--- src/main/java/org/foxesworld/engine/Engine.java
+++ src/main/java/org/foxesworld/engine/Engine.java
@@ -5,7 +5,6 @@ import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.core.config.Configurator;
-import org.foxesworld.APP;
 import org.foxesworld.cfgProvider.CfgProvider;
 import org.foxesworld.engine.action.ActionHandler;
 import org.foxesworld.engine.config.Config;
@@ -16,7 +15,6 @@ import org.foxesworld.engine.gui.styles.StyleAttributes;
 import org.foxesworld.engine.gui.styles.StyleProvider;
 import org.foxesworld.engine.locale.LanguageProvider;
 import org.foxesworld.engine.utils.FontUtils;
-import org.foxesworld.updater.Updater;
 
 import javax.swing.*;
 import java.awt.*;
@@ -47,7 +45,7 @@ public class Engine extends JFrame implements ActionListener {
     private Map<String, Map<String, StyleAttributes>> elementStyles = new HashMap<>();
     private final Frame frame;
     //private DownloadUtils download;
-    private Updater updater;
+    //private Updater updater;
     private final String[] configFiles = new String[]{ "internal/config"};
     private boolean init = false;
 
@@ -56,7 +54,7 @@ public class Engine extends JFrame implements ActionListener {
         this.engineData = new EngineData();
         initEngineValues("engine.json");
         System.setProperty("log.dir", CfgProvider.getGameFullPath());
-        LOGGER = LogManager.getLogger(APP.class);
+        LOGGER = LogManager.getLogger(Engine.class);
         LOGGER.info("Started "+engineData.getUpdaterBrand()+'-'+engineData.getUpdaterVersion());
         Configurator.setLevel(LOGGER.getName(), Level.valueOf("DEBUG"));
         this.config = new Config(this);
@@ -77,7 +75,7 @@ public class Engine extends JFrame implements ActionListener {
         this.loadMainPanel(this.app);
         this.actionHandler = new ActionHandler(this);
         //this.download = new DownloadUtils(this);
-        this.updater = new Updater(this);
+        //this.updater = new Updater(this);
         init = true;
     }
 
@@ -186,13 +184,8 @@ public class Engine extends JFrame implements ActionListener {
     public Config getConfig() {
         return config;
     }
-
     public void setEngineData(EngineData engineData) {
         this.engineData = engineData;
-    }
-
-    public Updater getUpdater() {
-        return updater;
     }
     public StyleProvider getStyleProvider() {
         return styleProvider;
