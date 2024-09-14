20a1fb7b010ebc0abf444bcee2a04937b7b1473f
Aiden <miomoor182@gmail.com>
2024-09-14 14:24:01 +0300

    ComponentType Fix

--- src/main/java/org/foxesworld/engine/gui/ComponentsAccessor.java
+++ src/main/java/org/foxesworld/engine/gui/ComponentsAccessor.java
@@ -13,13 +13,15 @@ public class ComponentsAccessor {
 
     private final GuiBuilder guiBuilder;
     private final String panelId;
+    private final List<Class<?>> componentTypes;
     private final Map<String, JComponent> componentMap = new HashMap<>();
     private final List<JComponent> componentList = new ArrayList<>();
     private final Map<String, String> formCredentials = new HashMap<>();
 
-    public ComponentsAccessor(GuiBuilder guiBuilder, String panelId) {
+    public ComponentsAccessor(GuiBuilder guiBuilder, String panelId, List<Class<?>> componentTypes) {
         this.guiBuilder = guiBuilder;
         this.panelId = panelId;
+        this.componentTypes = componentTypes;
         collectDataAndSelectComponents();
     }
 
@@ -29,36 +31,49 @@ public class ComponentsAccessor {
             for (JComponent component : components) {
                 String name = component.getName();
                 if (name != null && !name.isEmpty()) {
-                    componentMap.put(name, component);
-                    componentList.add(component);
-                    formCredentials.put(name, getValue(component));
+                    boolean isComponentType = false;
+                    for (Class<?> compType : this.componentTypes) {
+                        if (compType.isInstance(component)) {
+                            isComponentType = true;
+                            break;
+                        }
+                    }
+                    if (isComponentType) {
+                        componentMap.put(name, component);
+                        componentList.add(component);
+                        formCredentials.put(name, getValue(component));
+                    }
                 }
             }
         }
     }
-    private String getValue(JComponent component){
+
+    private String getValue(JComponent component) {
         String value = "";
         if (component instanceof JTextField) {
             value = ((JTextField) component).getText();
         } else if (component instanceof JCheckBox) {
             value = String.valueOf(((JCheckBox) component).isSelected());
-        } else if(component instanceof Slider){
+        } else if (component instanceof Slider) {
             value = String.valueOf(((Slider) component).getValue());
         }
 
-        return  value;
+        return value;
     }
+
     public Map<String, JComponent> getComponentMap() {
         return componentMap;
     }
+
     public List<JComponent> getComponentList() {
         return componentList;
     }
+
     public Map<String, String> getFormCredentials() {
         return formCredentials;
     }
+
     public JComponent getComponent(String id) {
         return componentMap.get(id);
     }
 }
-
